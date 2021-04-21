98850a37a4847e5286f5da067269f921fc8fee1b
ecal <ecallanan04@gmail.com>
2021-04-21 13:44:04 -0400

    Initial commit

---
+++ lua/plugins.lua
@@ -0,0 +1,17 @@
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  execute 'packadd packer.nvim'
+end
+
+return require('packer').startup(function()
+  -- Make packer manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- File explorer
+  use 'kyazdani42/nvim-tree.lua'
+end)
