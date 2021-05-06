e9ff065500aff96a9ff577e3f74289073e8f3410
ecal <ecallanan04@gmail.com>
2021-05-06 19:34:50 -0400

    Add ability to use xdg-open from nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,2 +1,42 @@
 vim.cmd('let g:nvim_tree_quit_on_open = 1')
 
+function NvimTreeXdgOpen()
+    local lib = require 'nvim-tree.lib'
+    local node = lib.get_node_at_cursor()
+    if node then
+        vim.fn.jobstart("xdg-open '" .. node.absolute_path .. "' &",
+                        {detach = true})
+    end
+end
+
+local tree_cb = require'nvim-tree.config'.nvim_tree_callback
+vim.g.nvim_tree_bindings = {
+    ["o"] = ":lua NvimTreeXdgOpen()<CR>",
+    ["<CR>"] = tree_cb("edit"),
+    ["<2-LeftMouse>"] = tree_cb("edit"),
+    ["<2-RightMouse>"] = tree_cb("cd"),
+    ["<C-]>"] = tree_cb("cd"),
+    ["<C-v>"] = tree_cb("vsplit"),
+    ["<C-x>"] = tree_cb("split"),
+    ["<C-t>"] = tree_cb("tabnew"),
+    ["<"] = tree_cb("prev_sibling"),
+    [">"] = tree_cb("next_sibling"),
+    ["<BS>"] = tree_cb("close_node"),
+    ["<S-CR>"] = tree_cb("close_node"),
+    ["<Tab>"] = tree_cb("preview"),
+    ["I"] = tree_cb("toggle_ignored"),
+    ["H"] = tree_cb("toggle_dotfiles"),
+    ["R"] = tree_cb("refresh"),
+    ["a"] = tree_cb("create"),
+    ["d"] = tree_cb("remove"),
+    ["r"] = tree_cb("full_rename"),
+    ["<C-r>"] = tree_cb("rename"),
+    ["x"] = tree_cb("cut"),
+    ["c"] = tree_cb("copy"),
+    ["p"] = tree_cb("paste"),
+    ["[c"] = tree_cb("prev_git_item"),
+    ["]c"] = tree_cb("next_git_item"),
+    ["-"] = tree_cb("dir_up"),
+    ["q"] = tree_cb("close")
+}
+
