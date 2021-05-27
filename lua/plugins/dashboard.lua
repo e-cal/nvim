afdda4d1afa8befe099098f89b729f0b7826a1e4
ecal <ecallanan04@gmail.com>
2021-05-27 13:58:14 -0400

    Change dashboard order

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,21 +1,33 @@
 vim.g.dashboard_custom_header = {
-'███████╗██╗   ██╗██╗███╗   ███╗',
-'██╔════╝██║   ██║██║████╗ ████║',
-'█████╗  ██║   ██║██║██╔████╔██║',
-'██╔══╝  ╚██╗ ██╔╝██║██║╚██╔╝██║',
-'███████╗ ╚████╔╝ ██║██║ ╚═╝ ██║',
-'╚══════╝  ╚═══╝  ╚═╝╚═╝     ╚═╝',
+    '███████╗██╗   ██╗██╗███╗   ███╗',
+    '██╔════╝██║   ██║██║████╗ ████║',
+    '█████╗  ██║   ██║██║██╔████╔██║',
+    '██╔══╝  ╚██╗ ██╔╝██║██║╚██╔╝██║',
+    '███████╗ ╚████╔╝ ██║██║ ╚═╝ ██║',
+    '╚══════╝  ╚═══╝  ╚═╝╚═╝     ╚═╝'
 }
 
 vim.g.dashboard_default_executive = 'telescope'
 
 vim.g.dashboard_custom_section = {
-    a = {description = {'  New File       '}, command = 'enew'},
-    b = {description = {'  Files          '}, command = 'Telescope find_files hidden=true'},
-    c = {description = {'  Find Text      '}, command = 'Telescope live_grep'},
+    e = {
+        description = {'  Restore Session'},
+        command = 'set showtabline=2 | SessionLoad'
+    },
+    b = {
+        description = {'  Files          '},
+        command = 'Telescope find_files hidden=true'
+    },
+    c = {
+        description = {'  Find Text      '},
+        command = 'Telescope live_grep'
+    },
     d = {description = {'  Recent Files   '}, command = 'Telescope oldfiles'},
-    e = {description = {'  Restore Session'}, command = 'set showtabline=2 | SessionLoad'},
-    f = {description = {'  Settings       '}, command = 'e ~/.config/nvim/settings.lua'}
+    a = {description = {'  New File       '}, command = 'enew'},
+    f = {
+        description = {'  Settings       '},
+        command = 'e ~/.config/nvim/settings.lua'
+    }
 }
 
 vim.g.dashboard_custom_footer = {'github.com/e-cal'}
