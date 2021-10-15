0ef4087ddcccbf1054f386310cdb405c86f7164b
ecal <ecallanan04@gmail.com>
2021-10-15 11:13:56 -0400

    Update nvim-tree config

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,3 +1,4 @@
+local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 vim.g.nvim_tree_quit_on_open = 1
 
 require'nvim-tree'.setup {
@@ -9,15 +10,52 @@ require'nvim-tree'.setup {
     open_on_tab = false,
     hijack_cursor = false,
     update_cwd = false,
-    lsp_diagnostics = false,
+    diagnostics = {enable = false},
     update_focused_file = {enable = false, update_cwd = false, ignore_list = {}},
     system_open = {cmd = nil, args = {}},
     view = {
         width = 30,
         side = 'left',
         auto_resize = false,
-        mappings = {custom_only = false, list = {}}
+        mappings = {
+            custom_only = false,
+            list = {
+                {key = "o", cb = ":lua NvimTreeXdgOpen()<CR>"},
+                {key = {"<CR>", "<2-LeftMouse>"}, cb = tree_cb("edit")},
+                {key = {"<2-RightMouse>", "<C-]>"}, cb = tree_cb("cd")},
+                {key = "v", cb = tree_cb("vsplit")},
+                {key = "h", cb = tree_cb("split")},
+                {key = "<C-t>", cb = tree_cb("tabnew")},
+                {key = "<", cb = tree_cb("prev_sibling")},
+                {key = ">", cb = tree_cb("next_sibling")},
+                {key = "P", cb = tree_cb("parent_node")},
+                {key = "<BS>", cb = tree_cb("close_node")},
+                {key = "<S-CR>", cb = tree_cb("close_node")},
+                {key = "<Tab>", cb = tree_cb("preview")},
+                {key = "K", cb = tree_cb("first_sibling")},
+                {key = "J", cb = tree_cb("last_sibling")},
+                {key = "I", cb = tree_cb("toggle_ignored")},
+                {key = "H", cb = tree_cb("toggle_dotfiles")},
+                {key = "R", cb = tree_cb("refresh")},
+                {key = "a", cb = tree_cb("create")},
+                {key = "d", cb = tree_cb("remove")},
+                {key = "r", cb = tree_cb("rename")},
+                {key = "<C-r>", cb = tree_cb("full_rename")},
+                {key = "x", cb = tree_cb("cut")},
+                {key = "c", cb = tree_cb("copy")},
+                {key = "p", cb = tree_cb("paste")},
+                {key = "y", cb = tree_cb("copy_name")},
+                {key = "Y", cb = tree_cb("copy_path")},
+                {key = "gy", cb = tree_cb("copy_absolute_path")},
+                {key = "[c", cb = tree_cb("prev_git_item")},
+                {key = "]c", cb = tree_cb("next_git_item")},
+                {key = "-", cb = tree_cb("dir_up")},
+                {key = "q", cb = tree_cb("close")},
+                {key = "g?", cb = tree_cb("toggle_help")}
+            }
+        }
     }
+
 }
 
 function NvimTreeXdgOpen()
@@ -28,34 +66,4 @@ function NvimTreeXdgOpen()
                         {detach = true})
     end
 end
-
-local tree_cb = require'nvim-tree.config'.nvim_tree_callback
-vim.g.nvim_tree_bindings = {
-    {key = "o", cb = ":lua NvimTreeXdgOpen()<CR>"},
-    {key = {"<CR>", "<2-LeftMouse>"}, cb = tree_cb("edit")},
-    {key = {"<2-RightMouse>", "<C-]>"}, cb = tree_cb("cd")},
-    {key = "v", cb = tree_cb("vsplit")}, {key = "h", cb = tree_cb("split")},
-    {key = "<C-t>", cb = tree_cb("tabnew")},
-    {key = "<", cb = tree_cb("prev_sibling")},
-    {key = ">", cb = tree_cb("next_sibling")},
-    {key = "P", cb = tree_cb("parent_node")},
-    {key = "<BS>", cb = tree_cb("close_node")},
-    {key = "<S-CR>", cb = tree_cb("close_node")},
-    {key = "<Tab>", cb = tree_cb("preview")},
-    {key = "K", cb = tree_cb("first_sibling")},
-    {key = "J", cb = tree_cb("last_sibling")},
-    {key = "I", cb = tree_cb("toggle_ignored")},
-    {key = "H", cb = tree_cb("toggle_dotfiles")},
-    {key = "R", cb = tree_cb("refresh")}, {key = "a", cb = tree_cb("create")},
-    {key = "d", cb = tree_cb("remove")}, {key = "r", cb = tree_cb("rename")},
-    {key = "<C-r>", cb = tree_cb("full_rename")},
-    {key = "x", cb = tree_cb("cut")}, {key = "c", cb = tree_cb("copy")},
-    {key = "p", cb = tree_cb("paste")}, {key = "y", cb = tree_cb("copy_name")},
-    {key = "Y", cb = tree_cb("copy_path")},
-    {key = "gy", cb = tree_cb("copy_absolute_path")},
-    {key = "[c", cb = tree_cb("prev_git_item")},
-    {key = "]c", cb = tree_cb("next_git_item")},
-    {key = "-", cb = tree_cb("dir_up")}, {key = "q", cb = tree_cb("close")},
-    {key = "g?", cb = tree_cb("toggle_help")}
-}
 
