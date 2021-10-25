79ea52ee2e89f0fe79106d0ad9f824448a8df54d
ecal <ecallanan04@gmail.com>
2021-10-25 10:21:37 -0400

    Update plugins

--- lua/plugins/doge.lua
+++
@@ -1,4 +0,0 @@
-vim.g.doge_mapping = "<leader>D"
-vim.g.doge_mapping_comment_jump_forward = "<C-j>"
-vim.g.doge_mapping_comment_jump_backward = "<C-k>"
-vim.g.doge_doc_standard_python = "google"
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,70 +1,77 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({
-        'git', 'clone', 'https://github.com/wbthomason/packer.nvim',
-        install_path
-    })
-    execute 'packadd packer.nvim'
+    fn.system(
+        {
+            "git",
+            "clone",
+            "https://github.com/wbthomason/packer.nvim",
+            install_path
+        }
+    )
+    execute "packadd packer.nvim"
 end
 
 -- Autocompile
-vim.cmd 'autocmd BufWritePost init.lua PackerCompile'
+vim.cmd "autocmd BufWritePost init.lua PackerCompile"
 
-return require('packer').startup(function(use)
-    -- Package Manager
-    use 'wbthomason/packer.nvim'
-    -- LSP
-    use 'neovim/nvim-lspconfig'
-    use 'glepnir/lspsaga.nvim'
-    use 'kabouzeid/nvim-lspinstall'
-    use 'SirVer/ultisnips'
-    -- Completion
-    use 'hrsh7th/nvim-cmp'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-nvim-lua'
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'kdheepak/cmp-latex-symbols'
-    use 'quangnguyen30192/cmp-nvim-ultisnips'
-    use 'f3fora/cmp-spell'
-    -- Navigation
-    use 'kyazdani42/nvim-tree.lua'
-    use 'nvim-lua/popup.nvim'
-    use 'nvim-lua/plenary.nvim'
-    use 'nvim-telescope/telescope.nvim'
-    use 'nvim-telescope/telescope-fzy-native.nvim'
-    use 'nvim-telescope/telescope-media-files.nvim'
-    -- Convenience
-    use 'folke/which-key.nvim'
-    use 'windwp/nvim-autopairs'
-    use 'terrortylor/nvim-comment'
-    use 'kevinhwang91/nvim-bqf'
-    use {'iamcco/markdown-preview.nvim', run = ':call mkdp#util#install()'}
-    use 'norcalli/nvim-colorizer.lua'
-    use 'lewis6991/gitsigns.nvim'
-    use 'tpope/vim-surround'
-    use 'akinsho/nvim-toggleterm.lua'
-    use 'windwp/nvim-ts-autotag'
-    use 'bfredl/nvim-ipy'
-    use 'lukas-reineke/indent-blankline.nvim'
-    use 'vhyrro/neorg'
-    -- Debugging
-    use 'mfussenegger/nvim-dap'
-    use 'rcarriga/nvim-dap-ui' -- temporary while nvim-dap lacks a built-in ui
-    use 'mfussenegger/nvim-dap-python'
-    use {'michaelb/sniprun', run = 'bash ./install.sh'}
-    -- Theming
-    use 'kyazdani42/nvim-web-devicons'
-    use 'glepnir/galaxyline.nvim'
-    use 'romgrk/barbar.nvim'
-    use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
-    use 'glepnir/dashboard-nvim'
-    use 'theniceboy/nvim-deus'
-    use 'arcticicestudio/nord-vim'
-    use 'rakr/vim-two-firewatch'
-    use 'sainnhe/edge'
-end)
+return require("packer").startup(
+    function(use)
+        -- Package Manager
+        use "wbthomason/packer.nvim"
+        -- LSP
+        use "neovim/nvim-lspconfig"
+        use "kabouzeid/nvim-lspinstall"
+        use "SirVer/ultisnips"
+        use "lukas-reineke/format.nvim"
+        use "ray-x/lsp_signature.nvim"
+        -- Completion
+        use "hrsh7th/nvim-cmp"
+        use "hrsh7th/cmp-buffer"
+        use "hrsh7th/cmp-path"
+        use "hrsh7th/cmp-nvim-lua"
+        use "hrsh7th/cmp-nvim-lsp"
+        use "kdheepak/cmp-latex-symbols"
+        use "quangnguyen30192/cmp-nvim-ultisnips"
+        use "f3fora/cmp-spell"
+        -- Navigation
+        use "kyazdani42/nvim-tree.lua"
+        use "nvim-lua/popup.nvim"
+        use "nvim-lua/plenary.nvim"
+        use "nvim-telescope/telescope.nvim"
+        use "nvim-telescope/telescope-fzy-native.nvim"
+        use "nvim-telescope/telescope-media-files.nvim"
+        -- Convenience
+        use "folke/which-key.nvim"
+        use "windwp/nvim-autopairs"
+        use "terrortylor/nvim-comment"
+        use "kevinhwang91/nvim-bqf"
+        use {"iamcco/markdown-preview.nvim", run = ":call mkdp#util#install()"}
+        use "norcalli/nvim-colorizer.lua"
+        use "lewis6991/gitsigns.nvim"
+        use "tpope/vim-surround"
+        use "akinsho/nvim-toggleterm.lua"
+        use "windwp/nvim-ts-autotag"
+        use "bfredl/nvim-ipy"
+        use "lukas-reineke/indent-blankline.nvim"
+        use "vhyrro/neorg"
+        -- Debugging
+        use "mfussenegger/nvim-dap"
+        use "rcarriga/nvim-dap-ui" -- temporary while nvim-dap lacks a built-in ui
+        use "mfussenegger/nvim-dap-python"
+        use {"michaelb/sniprun", run = "bash ./install.sh"}
+        -- Theming
+        use "kyazdani42/nvim-web-devicons"
+        use "glepnir/galaxyline.nvim"
+        use "romgrk/barbar.nvim"
+        use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
+        use "glepnir/dashboard-nvim"
+        use "theniceboy/nvim-deus"
+        use "arcticicestudio/nord-vim"
+        use "rakr/vim-two-firewatch"
+        use "sainnhe/edge"
+    end
+)
