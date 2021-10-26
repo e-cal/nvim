86f2d8e63b7cd9ec07e45aeb092513ecaa47bdfd
ecal <ecallanan04@gmail.com>
2021-10-26 16:46:39 -0400

    Update formatters, remove efm

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -1,18 +1,4 @@
 require "format".setup {
-    vim = {
-        {
-            cmd = {"luafmt -w replace"},
-            start_pattern = "^lua << EOF$",
-            end_pattern = "^EOF$"
-        }
-    },
-    vimwiki = {
-        {
-            cmd = {"prettier -w --parser babel"},
-            start_pattern = "^{{{javascript$",
-            end_pattern = "^}}}$"
-        }
-    },
     lua = {
         {
             cmd = {
@@ -26,16 +12,30 @@ require "format".setup {
             }
         }
     },
+    python = {
+        {cmd = {"black --fast"}}
+    },
     javascript = {
         {cmd = {"prettier -w", "./node_modules/.bin/eslint --fix"}}
     },
+    typescript = {
+        {cmd = {"prettier -w", "./node_modules/.bin/eslint --fix"}}
+    },
     markdown = {
-        {cmd = {"prettier -w"}},
+        {cmd = {"prettier -w"}}
+    },
+    vim = {
         {
-            cmd = {"black"},
-            start_pattern = "^```python$",
-            end_pattern = "^```$",
-            target = "current"
+            cmd = {"luafmt -w replace"},
+            start_pattern = "^lua << EOF$",
+            end_pattern = "^EOF$"
+        }
+    },
+    vimwiki = {
+        {
+            cmd = {"prettier -w --parser babel"},
+            start_pattern = "^{{{javascript$",
+            end_pattern = "^}}}$"
         }
     }
 }
