43e4f50ead3867e2d1c8439d2df8056d44803e41
ecal <ecallanan04@gmail.com>
2021-12-09 13:41:46 -0500

    Fix auto expand brackets

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,7 +1,14 @@
-require('nvim-autopairs').setup()
+local npairs = require("nvim-autopairs")
+local Rule = require("nvim-autopairs.rule")
 
 local remap = vim.api.nvim_set_keymap
-local npairs = require('nvim-autopairs')
+
+npairs.setup(
+    {
+        enable_check_bracket_line = true,
+        ignored_next_char = "[%w%.]" -- will ignore alphanumeric and `.` symbol
+    }
+)
 
 _G.MUtils = {}
 
@@ -18,14 +25,45 @@ MUtils.completion_confirm = function()
     end
 end
 
-remap('i', '<CR>', 'v:lua.MUtils.completion_confirm()',
-      {expr = true, noremap = true})
+remap(
+    "i",
+    "<CR>",
+    "v:lua.MUtils.completion_confirm()",
+    {expr = true, noremap = true}
+)
 
-local Rule = require('nvim-autopairs.rule')
-npairs.add_rules({
-    Rule(' ', ' '):with_pair(function(opts)
-        local pair = opts.line:sub(opts.col, opts.col + 2)
-        return vim.tbl_contains({'()', '[]', '{}'}, pair)
-    end)
-})
-
+npairs.add_rules {
+    Rule(" ", " "):with_pair(
+        function(opts)
+            local pair = opts.line:sub(opts.col - 1, opts.col)
+            return vim.tbl_contains({"()", "[]", "{}"}, pair)
+        end
+    ),
+    Rule("( ", " )"):with_pair(
+        function()
+            return false
+        end
+    ):with_move(
+        function(opts)
+            return opts.prev_char:match(".%)") ~= nil
+        end
+    ):use_key(")"),
+    Rule("{ ", " }"):with_pair(
+        function()
+            return false
+        end
+    ):with_move(
+        function(opts)
+            return opts.prev_char:match(".%}") ~= nil
+        end
+    ):use_key("}"),
+    Rule("[ ", " ]"):with_pair(
+        function()
+            return false
+        end
+    ):with_move(
+        function(opts)
+            return opts.prev_char:match(".%]") ~= nil
+        end
+    ):use_key("]")
+}
