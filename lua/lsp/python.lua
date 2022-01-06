87cad438de435c6d4f3db92d6dbe8d7ce9745cbc
ecal <ecallanan04@gmail.com>
2022-01-06 15:23:26 -0500

    Fix pyright config for normal install

--- lua/lsp/python.lua
+++ lua/lsp/python.lua
@@ -1,7 +1,3 @@
-require'lspconfig'.pyright.setup {
-    cmd = {
-        DATA_PATH .. "/lspinstall/python/node_modules/.bin/pyright-langserver",
-        "--stdio"
-    },
-    on_attach = require'lsp'.common_on_attach
+require "lspconfig".pyright.setup {
+    on_attach = require "lsp".common_on_attach
 }
