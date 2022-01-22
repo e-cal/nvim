2cf6d5fad9cd89976210250089c2fc51c05d5dc2
ecal <ecallanan04@gmail.com>
2022-01-22 15:28:14 -0500

    Fix lightspeed config

--- lua/plugins/lightspeed.lua
+++ lua/plugins/lightspeed.lua
@@ -1,7 +1,5 @@
 require "lightspeed".setup {
-    jump_on_partial_input_safety_timeout = 400,
-    highlight_unique_chars = true,
-    grey_out_search_area = true,
+    jump_to_unique_chars = true,
     match_only_the_start_of_same_char_seqs = true,
     limit_ft_matches = 5,
     substitute_chars = {["\r"] = "¬"},
