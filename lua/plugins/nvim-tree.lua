ffb943823d3d1be22f477447a43d92b3d6e3f890
ecal <ecallanan04@gmail.com>
2022-02-07 18:59:06 -0500

    Fix NvimTree highjacking everything

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -4,7 +4,7 @@ vim.g.nvim_tree_quit_on_open = 1
 require "nvim-tree".setup {
     disable_netrw = true,
     hijack_netrw = true,
-    open_on_setup = true,
+    open_on_setup = false,
     ignore_ft_on_setup = {},
     auto_close = false,
     open_on_tab = false,
