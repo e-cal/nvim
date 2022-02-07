c74b07b8518a13fa3279006f39d1122980e33925
ecal <ecallanan04@gmail.com>
2022-02-07 13:34:13 -0500

    Fix gitsigns config

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,28 +1,59 @@
-require('gitsigns').setup {
-  signs = {
-    add          = {hl = 'GitSignsAdd'   , text = '▎', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-    change       = {hl = 'GitSignsChange', text = '▎', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-    delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    topdelete    = {hl = 'GitSignsDelete', text = '▔', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    changedelete = {hl = 'GitSignsChange', text = '▎', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-  },
-  numhl = false,
-  linehl = false,
-  keymaps = {
-    ['n ]c'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'"},
-    ['n [c'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'"},
-
-    -- Text objects
-    ['o ih'] = ':<C-U>lua require"gitsigns".select_hunk()<CR>',
-    ['x ih'] = ':<C-U>lua require"gitsigns".select_hunk()<CR>'
+require("gitsigns").setup {
+    signs = {
+        add = {
+            hl = "GitSignsAdd",
+            text = "▎",
+            numhl = "GitSignsAddNr",
+            linehl = "GitSignsAddLn"
+        },
+        change = {
+            hl = "GitSignsChange",
+            text = "▎",
+            numhl = "GitSignsChangeNr",
+            linehl = "GitSignsChangeLn"
+        },
+        delete = {
+            hl = "GitSignsDelete",
+            text = "_",
+            numhl = "GitSignsDeleteNr",
+            linehl = "GitSignsDeleteLn"
+        },
+        topdelete = {
+            hl = "GitSignsDelete",
+            text = "▔",
+            numhl = "GitSignsDeleteNr",
+            linehl = "GitSignsDeleteLn"
+        },
+        changedelete = {
+            hl = "GitSignsChange",
+            text = "▎",
+            numhl = "GitSignsChangeNr",
+            linehl = "GitSignsChangeLn"
+        }
     },
-  watch_index = {
-    interval = 1000
-  },
-  current_line_blame = false,
-  sign_priority = 6,
-  update_debounce = 100,
-  status_formatter = nil, -- Use default
-  use_decoration_api = true,
-  use_internal_diff = true,  -- If luajit is present
+    numhl = false,
+    linehl = false,
+    keymaps = {
+        ["n ]c"] = {
+            expr = true,
+            '&diff ? \']c\' : \'<cmd>lua require"gitsigns".next_hunk()<CR>\''
+        },
+        ["n [c"] = {
+            expr = true,
+            '&diff ? \'[c\' : \'<cmd>lua require"gitsigns".prev_hunk()<CR>\''
+        },
+        -- Text objects
+        ["o ih"] = ':<C-U>lua require"gitsigns".select_hunk()<CR>',
+        ["x ih"] = ':<C-U>lua require"gitsigns".select_hunk()<CR>'
+    },
+    watch_gitdir = {
+        interval = 1000
+    },
+    current_line_blame = false,
+    sign_priority = 6,
+    update_debounce = 100,
+    status_formatter = nil, -- Use default
+    diff_opts = {
+        internal = true -- If luajit is present
+    }
 }
