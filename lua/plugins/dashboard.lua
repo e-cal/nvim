d407b8375eba2b910d92e00dfda88b932e13200d
ecal <ethan@ecal.dev>
2022-07-13 21:14:24 -0400

    feat: add file tree to dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -11,20 +11,24 @@ vim.g.dashboard_default_executive = "telescope"
 
 vim.g.dashboard_custom_section = {
 	a = {
+		description = { "פּ  File Tree       " },
+		command = "NvimTreeOpen",
+	},
+	b = {
 		description = { "  Files          " },
 		command = "Telescope find_files hidden=true",
 	},
-	b = { description = { "  Recent Files   " }, command = "Telescope oldfiles" },
-	c = { description = { "  New File       " }, command = "enew" },
-	d = {
+	c = { description = { "  Recent Files   " }, command = "Telescope oldfiles" },
+	d = { description = { "  New File       " }, command = "enew" },
+	e = {
 		description = { "  Find Text      " },
 		command = 'Telescope grep_string search="" only_sort_text=true',
 	},
-	e = {
+	f = {
 		description = { "  Restore Session" },
 		command = "set showtabline=2 | SessionLoad",
 	},
-	f = {
+	g = {
 		description = { "  Settings       " },
 		command = "e ~/.config/nvim/settings.lua",
 	},
