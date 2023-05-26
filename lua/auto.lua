d3a179094032798d836e6ce4d18d40d213131d7f
ecal <ethan@ecal.dev>
2023-05-26 09:35:16 -0400

    Cleanup auto commands

--- lua/auto.lua
+++ lua/auto.lua
@@ -2,37 +2,34 @@ local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
 augroup("global", { clear = true })
-
 autocmd({ "BufWinEnter", "BufRead", "BufNewFile" }, {
-  group = "global",
-  command = "setlocal formatoptions-=c formatoptions-=r formatoptions-=o formatoptions-=t",
+	group = "global",
+	command = "setlocal formatoptions-=c formatoptions-=r formatoptions-=o formatoptions-=t",
 })
 autocmd({ "VimLeave" }, {
-  group = "global",
-  callback = function()
-    vim.fn.jobstart("xclip -selection clipboard -i | " .. vim.api.nvim_exec("getreg('+')", true), { detach = true })
-  end,
+	group = "global",
+	callback = function()
+		vim.fn.jobstart("xclip -selection clipboard -i | " .. vim.api.nvim_exec("getreg('+')", true), { detach = true })
+	end,
 })
 autocmd({ "BufReadPost", "BufWritePost", "BufEnter", "TextChanged", "TextChangedI" }, {
-  group = "global",
-  command = "UpdateWinbarHighlight",
+	group = "global",
+	command = "UpdateWinbarHighlight",
 })
 
 augroup("markdown", { clear = true })
-
 autocmd({ "FileType" }, {
-  group = "markdown",
-  pattern = "markdown",
-  callback = function()
-    vim.cmd("setlocal spell foldexpr=MarkdownFold() foldmethod=expr nofoldenable ts=2 sts=2 sw=2")
-    vim.cmd('syntax match markdownIgnore "\\v\\w_\\w"')
-  end,
+	group = "markdown",
+	pattern = "markdown",
+	callback = function()
+		vim.cmd("setlocal spell foldexpr=MarkdownFold() foldmethod=expr nofoldenable ts=2 sts=2 sw=2")
+		vim.cmd('syntax match markdownIgnore "\\v\\w_\\w"')
+	end,
 })
 
--- augroup("python", { clear = true })
-
--- autocmd({ "Filetype" }, {
---   group = "python",
---   pattern = "python",
---   command = "setlocal indentkeys-=<:> indentkeys-=:",
--- })
+augroup("python", { clear = true })
+autocmd({ "Filetype" }, {
+	group = "python",
+	pattern = "python",
+	command = "setlocal indentkeys-=<:> indentkeys-=:",
+})
