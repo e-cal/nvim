2a3b3adb6a2a68fab93dfe818f4ec18626dfd166
ecal <ethan@ecal.dev>
2023-06-05 11:26:55 -0400

    Fullscreen tree on open dir

--- after/plugin/tree.lua
+++ after/plugin/tree.lua
@@ -153,7 +153,7 @@ require("neo-tree").setup({
 		follow_current_file = false, -- This will find and focus the file in the active buffer every
 		-- time the current file is changed while the tree is open.
 		group_empty_dirs = false, -- when true, empty folders will be grouped together
-		hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
+		hijack_netrw_behavior = "open_current", -- netrw disabled, opening a directory opens neo-tree
 		-- in whatever position is specified in window.position
 		-- "open_current",  -- netrw disabled, opening a directory opens within the
 		-- window like netrw would, regardless of window.position
