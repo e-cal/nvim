aebfa08b76ae8a89401e022acdeabb41ebbfbdaa
ecal <ethan@ecal.dev>
2023-12-18 17:03:57 -0500

    Remove old plugin config loading

--- init.lua
+++ init.lua
@@ -6,13 +6,5 @@ if not vim.g.vscode then
 	require("plugins")
 	require("functions")
 	require("auto")
-	require("colors")
 	require("lsp")
-
-	require("plugins.packer_compiled")
-	for _, file in ipairs(vim.fn.readdir(os.getenv("HOME") .. "/.config/nvim/lua/plugins")) do
-		if string.match(file, ".lua") and file ~= "packer_compiled.lua" then
-			require("plugins." .. string.gsub(file, ".lua", ""))
-		end
-	end
 end
