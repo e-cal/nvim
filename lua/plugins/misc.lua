4d8d6aadc70f8f59cf8d02427104aa90da359414
ecal <ethan@ecal.dev>
2024-11-22 16:13:44 -0500

    Disable sentiment in large files

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -11,8 +11,17 @@ return {
 		keys = { { "<leader>u", "<cmd>UndotreeToggle<cr>", desc = "undo tree" } },
 	},
 	{
-		"utilyre/sentiment.nvim",
+		"utilyre/sentiment.nvim", -- bracket pair matching
 		event = "VeryLazy",
+		enabled = function()
+			local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(0))
+			if ok and stats and stats.size > max_filesize then
+			    print("disabled sentiment (bracket matching) for large file")
+				return false
+			end
+			return true
+		end,
 		config = function()
 			require("sentiment").setup()
 			vim.cmd(
