4f84cec47b08416ca6955fd6561c65533c27d8a6
ecal <ethan@ecal.dev>
2025-02-06 13:39:20 -0500

    Telescope show gitignored files & load .telescopeignore

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,11 +15,39 @@ return {
 		pickers = {
 			find_files = {
 				follow = true,
+				hidden = true,
+				git_ignore = false,
+				file_ignore_patterns = { "^.git/" },
 			},
 		},
 	},
 	config = function(_, opts)
 		local actions = require("telescope.actions")
+
+		-- Custom function to load project ignores
+		local load_project_ignores = function()
+			local project_ignore = {}
+			local root = vim.fs.dirname(vim.fs.find(".git", { upward = true })[1] or "")
+			if root then
+				local ignore_file = root .. "/.telescopeignore"
+				if vim.fn.filereadable(ignore_file) == 1 then
+					for line in io.lines(ignore_file) do
+						if not line:match("^%s*#") and line ~= "" then
+							-- Convert glob patterns to Lua patterns
+							local pattern = line:gsub("[%%%[%]%^%$%(%)%.]", "%%%0"):gsub("%*", ".*"):gsub("%?", ".")
+							table.insert(project_ignore, pattern)
+						end
+					end
+				end
+			end
+			return project_ignore
+		end
+		-- Merge project ignores with defaults
+		local original_find_files = opts.pickers.find_files or {}
+		opts.pickers.find_files = vim.tbl_deep_extend("force", original_find_files, {
+			file_ignore_patterns = load_project_ignores(),
+		})
+
 		local extra_opts = {
 			defaults = {
 				mappings = {
