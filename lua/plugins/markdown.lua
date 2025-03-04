16e2140a61b1e7a086755ccb978893d0e810bad5
ecal <ethan@ecal.dev>
2025-03-04 12:47:29 -0500

    Build markdown-preview without node

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -2,11 +2,10 @@ return {
 	{
 		"iamcco/markdown-preview.nvim",
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		build = "cd app && npm install",
-		init = function()
-			vim.g.mkdp_filetypes = { "markdown" }
-		end,
 		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
 		keys = {
 			{ "<leader>mp", "<cmd>PreviewDoc<cr>", desc = "toggle preview" },
 			{ "<leader>ms", "1z=", desc = "fix spelling" },
