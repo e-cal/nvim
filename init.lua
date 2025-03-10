be54fa142ac0e751e5b1d86daa4390ebb34fe284
ecal <ethan@ecal.dev>
2025-03-10 18:20:16 -0400

    Add pyrepl and move stuff to lua/custom

--- init.lua
+++ init.lua
@@ -7,5 +7,14 @@ if not vim.g.vscode then
 	require("functions")
 	require("auto")
 	require("lsp")
-    require("sync")
+
+    -- include all files in lua/custom/
+	local dir = vim.fn.stdpath("config") .. "/lua/custom"
+	if vim.fn.isdirectory(dir) == 1 then
+		local files = vim.fn.glob(dir .. "/*.lua", false, true)
+		for _, f in ipairs(files) do
+			local module = vim.fn.fnamemodify(f, ":t:r")
+			require("custom." .. module)
+		end
+	end
 end
---
+++ lua/custom/pyrepl.lua
@@ -0,0 +1,69 @@
+local M = {}
+local curl = require("plenary.curl")
+
+M.config = {
+	url = "http://localhost:5000/execute",
+}
+
+function M.setup(opts)
+	M.config = vim.tbl_extend("force", M.config, opts or {})
+end
+
+function M.send_to_repl(code)
+	local response = curl.post(M.config.url, {
+		body = vim.fn.json_encode({ code = code }),
+		headers = {
+			content_type = "application/json",
+		},
+	})
+
+	local result = vim.fn.json_decode(response.body)
+end
+
+function M.get_visual_selection()
+    local _, srow, scol = unpack(vim.fn.getpos 'v')
+    local _, erow, ecol = unpack(vim.fn.getpos '.')
+
+	if vim.fn.mode() == "V" then
+		if srow > erow then
+			return vim.api.nvim_buf_get_lines(0, erow - 1, srow, true)
+		else
+			return vim.api.nvim_buf_get_lines(0, srow - 1, erow, true)
+		end
+	end
+
+	if vim.fn.mode() == "v" then
+		if srow < erow or (srow == erow and scol <= ecol) then
+			return vim.api.nvim_buf_get_text(0, srow - 1, scol - 1, erow - 1, ecol, {})
+		else
+			return vim.api.nvim_buf_get_text(0, erow - 1, ecol - 1, srow - 1, scol, {})
+		end
+	end
+end
+
+function M.run_selected_lines()
+	local code = M.get_visual_selection()
+	M.send_to_repl(code)
+end
+
+function M.run_current_line()
+    local line = vim.api.nvim_buf_get_lines(0, vim.fn.line('.') - 1, vim.fn.line('.'), true)
+    M.send_to_repl(line)
+end
+
+
+vim.keymap.set(
+	"v",
+	"<leader>pr",
+	M.run_selected_lines,
+	{ noremap = true, silent = true, desc = "Run in python REPL" }
+)
+
+vim.keymap.set(
+    "n",
+    "<leader>pr",
+    M.run_current_line,
+    { noremap = true, silent = true, desc = "Run current line in python REPL" }
+)
+
+return M
