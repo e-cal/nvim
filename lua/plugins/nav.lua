9854b8355723b7f874d644079680e800ea03be04
ecal <ethan@ecal.dev>
2025-06-16 16:42:18 -0400

    Tweak keybinds

--- lua/plugins/nav.lua
+++ lua/plugins/nav.lua
@@ -33,8 +33,8 @@ return {
         config = function()
             local harpoon = require("harpoon")
             harpoon:setup()
-            vim.keymap.set("n", "<leader>h", function() harpoon:list():add() end, { desc = "⇁ add" })
-            vim.keymap.set("n", "<leader>H", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "⇁ menu" })
+            vim.keymap.set("n", "<leader>p", function() harpoon:list():add() end, { desc = "⇁ pin" })
+            vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "⇁ list" })
 
             vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "⇁ 1" })
             vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "⇁ 2" })
