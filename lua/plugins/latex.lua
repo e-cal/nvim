ae514a3541cf6be1cd2e9dd20c5a5cef6ace5b90
ecal <ethan@ecal.dev>
2025-10-11 12:08:10 -0400

    Disable auto open on latex compile

--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -6,6 +6,8 @@ return {
 	},
 	init = function()
 		vim.g.vimtex_view_method = "zathura"
+
+        vim.g.vimtex_view_automatic = 0
 		vim.g.vimtex_view_forward_search_on_start = 0
         vim.g.vimtex_quickfix_enabled = 0
 
