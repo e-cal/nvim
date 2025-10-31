dd287fbe9f0b9f85223d58d5eedadedb543c99c7
ecal <ethan@ecal.dev>
2025-10-31 15:18:20 -0400

    Switch to copilot (rip supermaven)

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,44 @@
+return {
+	{
+		"copilotlsp-nvim/copilot-lsp",
+		init = function()
+			vim.g.copilot_nes_debounce = 500
+			vim.lsp.enable("copilot_ls")
+			vim.keymap.set("n", "<tab>", function()
+				local bufnr = vim.api.nvim_get_current_buf()
+				local state = vim.b[bufnr].nes_state
+				if state then
+					-- Try to jump to the start of the suggestion edit.
+					-- If already at the start, then apply the pending suggestion and jump to the end of the edit.
+					local _ = require("copilot-lsp.nes").walk_cursor_start_edit()
+						or (
+							require("copilot-lsp.nes").apply_pending_nes()
+							and require("copilot-lsp.nes").walk_cursor_end_edit()
+						)
+					return nil
+				else
+					-- Resolving the terminal's inability to distinguish between `TAB` and `<C-i>` in normal mode
+					return "<C-i>"
+				end
+			end, { desc = "Accept Copilot NES suggestion", expr = true })
+		end,
+	},
+	{
+		"zbirenbaum/copilot.lua",
+		requires = { "copilotlsp-nvim/copilot-lsp" },
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({
+				nes = {
+					enabled = true,
+					keymap = {
+						accept_and_goto = "<C-j>",
+						accept = false,
+						dismiss = "<Esc>",
+					},
+				},
+			})
+		end,
+	},
+}
