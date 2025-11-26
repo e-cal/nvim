f02d810f0ac78405bcba778e123694c18815c62e
ecal <ethan@ecal.dev>
2025-11-26 13:57:31 -0500

    No background

--- lua/custom/theme.lua
+++ lua/custom/theme.lua
@@ -2,3 +2,9 @@ vim.api.nvim_set_hl(0, "TreesitterContextBottom", {
 	underline = true,
 	sp = Utils.brightness_modifier(Utils.get_hl("Normal", "bg"), 100),
 })
+
+vim.api.nvim_set_hl(0, "Normal", { bg = nil })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = nil })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
