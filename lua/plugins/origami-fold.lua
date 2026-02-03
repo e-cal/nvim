484f5ffca5cb6efd946284b573a4b0716fc96b06
Ethan Callanan <ethanc@evolver.ai>
2026-02-03 18:20:21 -0500

    Better fold keymaps

--- lua/plugins/origami-fold.lua
+++ lua/plugins/origami-fold.lua
@@ -2,7 +2,14 @@ return {
 	"chrisgrieser/nvim-origami",
 	event = "VeryLazy",
 	opts = {
-        autoFold = { enabled = false  },
+		autoFold = { enabled = false },
+		foldKeymaps = { setup = false },
+	},
+    keys = {
+        { "H", function() require("origami").h() end, desc = "Fold: Move h (with peek)" },
+        { "L", function() require("origami").l() end, desc = "Fold: Move l (with peek)" },
+        { "$", function() require("origami").dollar() end, desc = "Fold: End of line (with peek)" },
+        { "^", function() require("origami").caret() end, desc = "Fold: Start of line (with peek)" },
     },
 
 	init = function()
