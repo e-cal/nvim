0016ee43661fd3a97a5c710c38afe20e6c3545ae
ecal <ethan@ecal.dev>
2026-05-11 09:48:04 -0400

    Allow multi markdown preview

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -5,7 +5,7 @@ return {
 		config = function()
 			require("markdown_preview").setup({
 				-- all optional; sane defaults shown
-				instance_mode = "takeover", -- "takeover" (one tab) or "multi" (tab per instance)
+				instance_mode = "multi", -- "takeover" (one tab) or "multi" (tab per instance)
 				port = 0, -- 0 = auto (8421 for takeover, OS-assigned for multi)
 				open_browser = true,
 				debounce_ms = 300,
